ed3159ef30d788e430ae08bee660398b39f30efc
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2021-02-10 11:09:01 +0300

    Завершает мобильное меню и главный блок

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
 (function () {
-  return;
+  const nav = document.querySelector(`.main-header__navigation`);
+  const toggle = document.querySelector(`.main-header__navigation button[type="button"]`);
+
+  nav.classList.remove(`main-header__navigation--nojs`);
+
+  toggle.addEventListener(`click`, () => {
+    if (nav.classList.contains(`main-header__navigation--closed`)) {
+      nav.classList.remove(`main-header__navigation--closed`);
+      document.body.classList.add(`page__body--fullscreen`);
+    } else {
+      nav.classList.add(`main-header__navigation--closed`);
+      document.body.classList.remove(`page__body--fullscreen`);
+    }
+  });
 })();
