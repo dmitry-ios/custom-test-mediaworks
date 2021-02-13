72aa3c976852bc5a06d62223c0ffd6ae7094e6a2
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2021-02-13 10:05:25 +0300

    Вносит мелкие правки

--- source/js/login.js
+++ source/js/login.js
@@ -1,19 +1,18 @@
 'use strict';
 
-(function () {
+(() => {
   const button = document.querySelector(`#enter-login-button`);
   const modal = document.querySelector(`.login`);
   const closeButton = document.querySelector(`#close-button`);
 
-  const onCloseModal = function () {
+  const onCloseModal = () => {
     modal.classList.add(`login--closed`);
     closeButton.removeEventListener(`click`, onCloseModal);
   };
 
-  button.addEventListener(`click`, function (evt) {
+  button.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     modal.classList.remove(`login--closed`);
     closeButton.addEventListener(`click`, onCloseModal);
   });
-
 })();
--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-(function () {
+(() => {
+  const OPEN_TEXT = `Открыть меню`;
+  const CLOSE_TEXT = `Закрыть меню`;
+
   const nav = document.querySelector(`.main-header__navigation`);
   const toggle = document.querySelector(`.main-header__navigation button[type="button"]`);
+  const toggleText = toggle.querySelector(`span`);
 
   nav.classList.remove(`main-header__navigation--nojs`);
 
@@ -10,9 +14,11 @@
     if (nav.classList.contains(`main-header__navigation--closed`)) {
       nav.classList.remove(`main-header__navigation--closed`);
       document.body.classList.add(`page__body--fullscreen`);
+      toggleText.textContent = CLOSE_TEXT;
     } else {
       nav.classList.add(`main-header__navigation--closed`);
       document.body.classList.remove(`page__body--fullscreen`);
+      toggleText.textContent = OPEN_TEXT;
     }
   });
 })();
